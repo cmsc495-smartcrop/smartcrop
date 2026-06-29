c6d422ebe86e80bf649880614c3f83b705d097a5
comsma <cmsmallegan@comsma.com>
2026-06-29 17:29:25 -0500

    added route for station

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -5,10 +5,23 @@ import "github.com/labstack/echo/v5"
 func registerHandlers(e *echo.Echo) {
 	h := &Handler{}
 	e.GET("/", h.Home)
+	e.GET("/station/:id", h.StationView)
 }
 
 type Handler struct{}
 
 func (h *Handler) Home(c *echo.Context) error {
 	return c.Render(200, "pages/home.gohtml", nil)
+}
+
+type StationView struct {
+	StationID       string  `json:"station_id"`
+	Temperature     float64 `json:"temperature"`
+	TemperatureUnit string  `json:"temperature_unit"`
+	Humidity        float64 `json:"humidity"`
+}
+
+func (h *Handler) StationView(c *echo.Context) error {
+	stationID := c.Param("id")
+	return c.Render(200, "pages/station.gohtml", StationView{StationID: stationID, Temperature: 70.0, TemperatureUnit: "F", Humidity: 50.0})
 }
